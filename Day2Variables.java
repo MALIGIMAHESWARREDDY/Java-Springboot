a4c30242f09d86a58597b4fb5da7f86f89fb5c11
maheswarreddy <162997868+MALIGIMAHESWARREDDY@users.noreply.github.com>
2025-09-15 09:34:33 +0530

    day2

---
+++ Day2Variables.java
@@ -0,0 +1,78 @@
+class Variables {
+
+    public static void main(String[] args){
+        // variables
+
+
+        int nooflikes = 1000;
+        float pricePerkg = 72.4f;
+        char singlechar = 'm';
+        boolean isliked = true;
+        boolean isdisliked = false;
+
+        System.out.println(nooflikes);
+        System.out.println(pricePerkg);
+        System.out.println(singlechar);
+        System.out.println(isliked);
+        System.out.println(isdisliked);
+// charactor or length of the user Name
+        String userName = "maligi maheswarreddy";
+        System.out.println(userName);
+        userName = "maligi";
+        System.out.println(userName);
+        System.out.println(userName.length());
+//unwanted spacing remove
+        userName = " maligi  maheswarreddy ";
+        System.out.println(userName.trim());
+        System.out.println(userName.length());
+// first name and lastname adding
+        var userFirstName = "maligi";
+        var userSecondName = "reddy";
+        System.out.println(userFirstName+" "+userSecondName);
+// coupon code validation
+        String orgCouponcode = "FIRST50";
+        String userCouponcode = "first50";
+
+        System.out.println(orgCouponcode.toLowerCase()) ;
+        System.out.println(userCouponcode.toLowerCase()) ;
+
+        orgCouponcode = orgCouponcode.toLowerCase();
+        System.out.println(orgCouponcode .equals(userCouponcode));
+
+        // Try different string methods on your name, email, state and country.
+
+        String Name = "maligi maheswar reddy";
+        Name = "Maligi";
+        System.out.println(Name.length());
+
+        Name = " maligi maheswar reddy ";
+        System.out.println(Name.length());
+        System.out.println(Name.trim());
+
+        String firstName ="maligi";
+        String lastName ="mahesh";
+        System.out.println(firstName+ " " +lastName);
+        String fullName = "maligi maheswar reddy";
+        System.out.println(fullName);
+
+
+        // gmail
+
+        String userEmail = "maligimaheswarreddy@gmail.com";
+        userEmail=" maligimaheswarreddy@gmail.com ";
+        System.out.println(userEmail);
+        System.out.println(userEmail.length());
+        System.out.println(userEmail.trim());
+
+        // state country
+
+        String userState= "ap";
+        System.out.println(userState);
+        String userAdress1 = "reddyvaripalli";
+        String userAdress2 = "vepurikota";
+        String userAdress3 = "517351,india";
+
+        System.out.println(userAdress1+ "," +userAdress2 +","+userAdress3);
+
+    }
+}
