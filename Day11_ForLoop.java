ae19e5e31cdc9d8d20324efaad5dc33f411d15a3
maheswarreddy <162997868+MALIGIMAHESWARREDDY@users.noreply.github.com>
2025-10-03 22:09:12 +0530

    for loop

---
+++ Day11_ForLoop.java
@@ -0,0 +1,11 @@
+class Loops{
+    public static void main (String []args){
+        String [] titles = {"iphone14 review","iphone15 review","iphone16 review"};
+
+        for(int i = 0; i < titles.length; i++){
+            System.out.println(titles[i]);
+        }
+    }
+}
+
+
