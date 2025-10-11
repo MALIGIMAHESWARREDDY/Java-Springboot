d7a8c1c3ba3ec2f653306f4097543f0ec16c67d5
maheswarreddy <162997868+MALIGIMAHESWARREDDY@users.noreply.github.com>
2025-10-11 14:09:25 +0530

    Inheritance

---
+++ Day15_Inheritance.java
@@ -0,0 +1,14 @@
+class Inhertitance{
+    String email = "maheswarreddy@gmail.com";
+    String password = "mahesh123@";
+    public static void main(String[]args){
+        System.out.println("email");
+    }
+    public void login(){
+        System.out.println("login sucessfully done");
+    }
+    public void Signup(){
+        System.out.println("Account created sucessfully");
+    }
+    
+}
