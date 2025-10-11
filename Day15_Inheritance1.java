397e7081ce22561c63510b18d2316379ea6475d0
maheswarreddy <162997868+MALIGIMAHESWARREDDY@users.noreply.github.com>
2025-10-11 14:10:29 +0530

    Inheritance,child class

---
+++ Day15_Inheritance1.java
@@ -0,0 +1,11 @@
+class Inhertitance1 extends Inhertitance {
+    public static void main(String[]args){
+        Inhertitance1 obj5 = new Inhertitance1();
+        obj5.login();
+        obj5.Signup();
+        obj5.RestPassword();
+    }
+    public void RestPassword(){
+        System.out.println("restpassword");
+    }
+}
